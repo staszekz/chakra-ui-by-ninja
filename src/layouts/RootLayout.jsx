1a1445126579d10feec90d945e9c5af05a62a158
staszekZ <staszek.zajaczkowski@gmail.com>
2023-07-10 22:58:43 +0200

    added custom  theme

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -6,7 +6,7 @@ import {Grid, GridItem} from "@chakra-ui/react";
 export default function RootLayout() {
   return (
     <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
-      <GridItem as="aside" colSpan={{base: 6, lg: 2, xl: 1}} bg="purple.400" minHeight={{lg: "100vh"}} p="30px">
+      <GridItem as="aside" colSpan={{base: 6, lg: 2, xl: 1}} bg="brand.500" minHeight={{lg: "100vh"}} p="30px">
        <Sidebar/>
       </GridItem>
       <GridItem as="main" colSpan={{base: 6, lg: 4, xl: 5}} p={{base: '20px', lg:"30px"}} >
