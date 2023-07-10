c551d5b968590387c3865764ccdb5b56dbeea87f
staszekZ <staszek.zajaczkowski@gmail.com>
2023-07-10 16:55:59 +0200

    instralled icons and added lists

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,12 +1,13 @@
 import {Outlet} from "react-router-dom";
 import Navbar from '../components/Navbar';
+import Sidebar from '../components/SIdebar'
 import {Grid, GridItem} from "@chakra-ui/react";
 
 export default function RootLayout() {
   return (
     <Grid templateColumns="repeat(6, 1fr)" bg="gray.50">
       <GridItem as="aside" colSpan={{base: 6, lg: 2, xl: 1}} bg="purple.400" minHeight={{lg: "100vh"}} p="30px">
-        <span>sidebar</span>
+       <Sidebar/>
       </GridItem>
       <GridItem as="main" colSpan={{base: 6, lg: 4, xl: 5}} p={{base: '20px', lg:"30px"}} >
         <Navbar />
